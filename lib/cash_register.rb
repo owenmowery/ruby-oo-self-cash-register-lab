5adaf11ebdd6a91b52d88d7602532fb29170463f
Owen Mowery <owenmowery99@gmail.com>
2021-03-03 16:57:55 -0500

    commit tree 4e1def461b93e52018c646991a526a36ca7fab60

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+require 'pry'
+class CashRegister
 
+    attr_accessor :total, :discount, :items, :last_item
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        @last_item = price * quantity
+        @total += (price * quantity)
+        quantity.times { @items.push(title) }
+    end
+
+    def apply_discount
+        if @discount > 0
+            percent_discount = @discount / 100.0
+            @total = @total - (@total * percent_discount).to_i
+            return "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        void_item = @items.pop(1)
+        @total -= @last_item
+      end
+
+
+end
